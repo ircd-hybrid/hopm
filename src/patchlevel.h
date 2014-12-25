1f78ecadbc5323f132c9b21a69b26b51fafd6912
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-12-25 17:51:12 +0000

    - Added patchlevel.h
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/trunk@5129 82007160-df01-0410-b94d-b575c5fd34c7

---
+++ src/patchlevel.h
@@ -0,0 +1,8 @@
+/*! \file patchlevel.h
+ * \brief A header defining the patchlevel.
+ * \version $Id: patchlevel.h 4772 2014-10-20 18:04:30Z michael $
+ */
+
+#ifndef PATCHLEVEL
+#define PATCHLEVEL   "hopm-1.0.0beta1"
+#endif
