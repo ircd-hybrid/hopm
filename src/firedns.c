fee08a4079a8405a8896d20f18865cc35936ffdd
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-12-31 14:42:24 +0000

    - scan.c: use poll.h; removed useless cast - firedns.c: use poll.h
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/trunk@5227 82007160-df01-0410-b94d-b575c5fd34c7

--- src/firedns.c
+++ src/firedns.c
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/poll.h>
+#include <poll.h>
 #include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
