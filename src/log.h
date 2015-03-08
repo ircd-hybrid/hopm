9eb71ae1faf1ce0cfaf607d14eb80937289d7439
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-03-08 13:15:45 +0000

    - log.c, log.h: constification
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/trunk@5647 82007160-df01-0410-b94d-b575c5fd34c7

--- src/log.h
+++ src/log.h
@@ -21,9 +21,9 @@
 #ifndef LOG_H
 #define LOG_H
 
-extern void log_open(char *);
+extern void log_open(const char *);
 extern void log_close(void);
-extern void scanlog_open(char *);
+extern void scanlog_open(const char *);
 extern void scanlog_close(void);
 #ifdef __GNUC__
 extern void log_printf(const char *data, ...) __attribute__((format(printf,1,2)));
