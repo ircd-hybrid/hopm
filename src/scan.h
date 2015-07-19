791245e2c1f8187324ef26a3338d4ffcc13b23d7
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-07-19 12:00:35 +0000

    - scan.c, scan.h: removed prototype for scan_checkexempt() and declared it as static
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/trunk@6300 82007160-df01-0410-b94d-b575c5fd34c7

--- src/scan.h
+++ src/scan.h
@@ -58,7 +58,6 @@ extern void scan_cycle(void);
 extern void scan_connect(const char *[], const char *);
 extern void scan_checkfinished(struct scan_struct *);
 extern void scan_manual(char *, const struct ChannelConf *);
-extern int scan_checkexempt(const char *, const char *);
 extern void scan_timer(void);
 extern void scan_positive(struct scan_struct *, const char *, const char *);
 #endif /* SCAN_H */
