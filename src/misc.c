01e751d2be530495b8cc11a6f49fdd8f106467c8
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-10-13 17:40:47 +0000

    - misc.c: change dissect_time() to always return a "%u day%s, %02u:%02u:%02u" format
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/trunk@6568 82007160-df01-0410-b94d-b575c5fd34c7

--- src/misc.c
+++ src/misc.c
@@ -34,89 +34,29 @@ const char *
 dissect_time(time_t time)
 {
   static char buf[64];
-  unsigned int years, weeks, days, hours, minutes, seconds;
-
-  years = weeks = days = hours = minutes = seconds = 0;
-
-  while (time >= 60 * 60 * 24 * 365)
-  {
-    time -= 60 * 60 * 24 * 365;
-    years++;
-  }
-
-  while (time >= 60 * 60 * 24 * 7)
-  {
-    time -= 60 * 60 * 24 * 7;
-    weeks++;
-  }
+  unsigned int days = 0, hours = 0, minutes = 0, seconds = 0;
 
   while (time >= 60 * 60 * 24)
   {
     time -= 60 * 60 * 24;
-    days++;
+    ++days;
   }
 
   while (time >= 60 * 60)
   {
     time -= 60 * 60;
-    hours++;
+    ++hours;
   }
 
   while (time >= 60)
   {
     time -= 60;
-    minutes++;
+    ++minutes;
   }
 
   seconds = time;
 
-  if (years)
-  {
-    snprintf(buf, sizeof(buf),
-             "%d year%s, %d week%s, %d day%s, %02d:%02d:%02d",
-             years, years == 1 ? "" : "s", weeks,
-             weeks == 1 ? "" : "s", days, days == 1 ? "" : "s",
-             hours, minutes, seconds);
-  }
-  else if (weeks)
-  {
-    snprintf(buf, sizeof(buf),
-             "%d week%s, %d day%s, %02d:%02d:%02d", weeks,
-             weeks == 1 ? "" : "s", days, days == 1 ? "" : "s",
-             hours, minutes, seconds);
-  }
-  else if (days)
-  {
-    snprintf(buf, sizeof(buf), "%d day%s, %02d:%02d:%02d",
-             days, days == 1 ? "" : "s", hours, minutes, seconds);
-  }
-  else if (hours)
-  {
-    if (minutes || seconds)
-    {
-      snprintf(buf, sizeof(buf),
-               "%d hour%s, %d minute%s, %d second%s", hours,
-               hours == 1 ? "" : "s", minutes,
-               minutes == 1 ? "" : "s", seconds,
-               seconds == 1 ? "" : "s");
-    }
-    else
-    {
-      snprintf(buf, sizeof(buf), "%d hour%s", hours,
-               hours == 1 ? "" : "s");
-    }
-  }
-  else if (minutes)
-  {
-    snprintf(buf, sizeof(buf), "%d minute%s, %d second%s",
-             minutes, minutes == 1 ? "" : "s", seconds,
-             seconds == 1 ? "" : "s");
-  }
-  else
-  {
-    snprintf(buf, sizeof(buf), "%d second%s", seconds,
-            seconds == 1 ? "" : "s");
-  }
-
+  snprintf(buf, sizeof(buf), "%u day%s, %02u:%02u:%02u",
+           days, days == 1 ? "" : "s", hours, minutes, seconds);
   return buf;
 }
