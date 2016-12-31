15f6567aa566c1947adc086dd61f924c721f02bb
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2016-12-31 13:59:33 +0000

    - Update copyright years
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/branches/1.1.x@7926 82007160-df01-0410-b94d-b575c5fd34c7

--- src/config-lexer.c
+++ src/config-lexer.c
@@ -799,7 +799,7 @@ char *yytext;
 #line 1 "config-lexer.l"
 /*
  *  Copyright (c) 2002 Erik Fears
- *  Copyright (c) 2014-2016 ircd-hybrid development team
+ *  Copyright (c) 2014-2017 ircd-hybrid development team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
