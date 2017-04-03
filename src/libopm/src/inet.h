7e04e46ee8d88924b98719f7357c5b62394bf18d
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2017-04-03 18:26:06 +0000

    - Get rid of the _opm_sockaddr and _opm_inaddr structs
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/branches/1.1.x@8131 82007160-df01-0410-b94d-b575c5fd34c7

--- src/libopm/src/inet.h
+++ src/libopm/src/inet.h
@@ -9,15 +9,4 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
-typedef struct _opm_sockaddr opm_sockaddr;
-typedef struct _opm_inaddr opm_inaddr;
-
-struct _opm_sockaddr {
-        struct sockaddr_in sa4;
-};
-
-struct _opm_inaddr {
-        struct in_addr in4;
-};
-
 #endif /* INET_H */
