5fa5ac8f5c5e69e0e2b127445d0d169f099a6408
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2017-04-15 21:54:36 +0000

    - Rename functions in memory.c to be prefixed with libopm_
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/branches/1.1.x@8206 82007160-df01-0410-b94d-b575c5fd34c7

--- src/libopm/src/memory.c
+++ src/libopm/src/memory.c
@@ -1,27 +1,22 @@
 /*
- * Copyright (C) 2002  Erik Fears
+ *  Copyright (c) 2002 Erik Fears
+ *  Copyright (c) 2014-2017 ircd-hybrid development team
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to
- *
- *       The Free Software Foundation, Inc.
- *       59 Temple Place - Suite 330
- *       Boston, MA  02111-1307, USA.
- *
- *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ *  USA
  */
-
-#include "setup.h"
 
 #include <stdlib.h>
 #include <assert.h>
@@ -30,49 +25,44 @@
 #include "memory.h"
 
 
-/* xcalloc
- *  
+/* libopm_calloc
+ *
  *   A wrapper function for malloc(), for catching memory issues
  *   and error handling.
  *
  * Parameters
  *    bytes: amount in bytes to allocate
- *    
+ *
  * Return:
  *    Pointer to allocated memory
  */
-
-void *libopm_xcalloc(size_t bytes)
+void *
+libopm_calloc(size_t bytes)
 {
    void *ret = calloc(1, bytes);
-
-   assert(ret); 
+   assert(ret);
 
    return ret;
 }
 
-
-
-/*  MyFree
+/* libopm_free
  *
  *  Free memory allocated with xcalloc
  *
  *  Parameters:
  *     var: pointer to memory to free
- *     
+ *
  *  Return:
  *     None
  */
-
-void libopm_MyFree(void **var)
+void
+libopm_free(void *ptr)
 {
-   if(*var != NULL)
-      free(*var);
-   *var = NULL;
+  free(ptr);
 }
 
 void *
-libopm_xstrdup(const char *s)
+libopm_strdup(const char *s)
 {
   void *ret = malloc(strlen(s) + 1);
 
--- src/libopm/src/memory.h
+++ src/libopm/src/memory.h
@@ -1,11 +1,28 @@
+/*
+ *  Copyright (c) 2002 Erik Fears
+ *  Copyright (c) 2014-2017 ircd-hybrid development team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ *  USA
+ */
+
 #ifndef MALLOC_H
 #define MALLOC_H
 
-#define xcalloc(SIZE) libopm_xcalloc(SIZE)
-#define MyFree(X) libopm_MyFree((void **) &X)
-
-extern void *libopm_xcalloc(size_t bytes);
-extern void libopm_MyFree(void **var);
-extern void *libopm_xstrdup(const char *);
+extern void *libopm_calloc(size_t);
+extern void libopm_free(void *);
+extern void *libopm_strdup(const char *);
 
 #endif /* MALLOC_H */
