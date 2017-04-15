3b0217f9903bbe28f344ccf8bb29135a89e8f319
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2017-04-15 18:28:55 +0000

    - Update libopm list manipulation routines
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/trunk@8200 82007160-df01-0410-b94d-b575c5fd34c7

--- src/libopm/src/list.c
+++ src/libopm/src/list.c
@@ -1,27 +1,32 @@
 /*
- * Copyright (C) 2002-2003  Erik Fears
+ *  Copyright (c) 2002-2003 Erik Fears
+ *  Copyright (c) 2014-2017 ircd-hybrid development team
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to
- *
- *       The Free Software Foundation, Inc.
- *       59 Temple Place - Suite 330
- *       Boston, MA  02111-1307, USA.
- *
- *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ *  USA
+ */
+
+/*! \file list.c
+ * \brief Maintains doubly-linked lists.
+ * \version $Id$
  */
 
 #include "setup.h"
+
+#include <stdlib.h>
+#include <assert.h>
 
 #include "opm_common.h"
 #include "list.h"
@@ -29,90 +34,78 @@
 #include "opm.h"
 
 
-OPM_NODE_T *libopm_node_create(void *data)
+OPM_NODE_T *
+libopm_node_create(void *data)
 {
-   OPM_NODE_T *node = xcalloc(sizeof *node);
-   node->next = NULL;
-   node->prev = NULL;  
-   node->data = (void *) data;
+  OPM_NODE_T *node = xcalloc(sizeof *node);
 
-   return node;
+  node->data = data;
+
+  return node;
 }
 
-OPM_LIST_T *libopm_list_create()
+OPM_LIST_T *
+libopm_list_create(void)
 {
-   OPM_LIST_T *list = xcalloc(sizeof *list);
-
-   list->head = NULL;
-   list->tail = NULL;
-
-   list->elements = 0;
-
-   return list;
+  OPM_LIST_T *list = xcalloc(sizeof *list);
+  return list;
 }
 
-OPM_NODE_T *libopm_list_add(OPM_LIST_T *list, OPM_NODE_T *node)
+OPM_NODE_T *
+libopm_list_add(OPM_LIST_T *list, OPM_NODE_T *node)
 {
+  node->prev = NULL;
+  node->next = list->head;
 
-   if(list == NULL || node == NULL)
-      return NULL;   
+  /* Assumption: If list->tail != NULL, list->head != NULL */
+  if (list->head)
+    list->head->prev = node;
+  else if (list->tail == NULL)
+    list->tail = node;
 
-   if(list->tail == NULL)
-   {
-      list->head = node;
-      list->tail = node;
+  list->head = node;
+  list->elements++;
 
-      node->next = NULL;
-      node->prev = NULL;
-   }
-   else
-   {
-      node->prev = list->tail;
-      list->tail->next = node;
-      list->tail = node;
-      node->next = NULL;
-   }
-
-   list->elements++;
-   return node;
+  return node;
 }
 
-OPM_NODE_T *libopm_list_remove(OPM_LIST_T *list, OPM_NODE_T *node)
+OPM_NODE_T *
+libopm_list_remove(OPM_LIST_T *list, OPM_NODE_T *node)
 {
+  /* Assumption: If node->next == NULL, then list->tail == node
+   *      and:   If node->prev == NULL, then list->head == node
+   */
+  if (node->next)
+    node->next->prev = node->prev;
+  else
+  {
+    assert(list->tail == node);
+    list->tail = node->prev;
+  }
 
-   if(list == NULL || node == NULL)
-      return NULL;
+  if (node->prev)
+    node->prev->next = node->next;
+  else
+  {
+    assert(list->head == node);
+    list->head = node->next;
+  }
 
-   if(node == list->head)
-   {
-      list->head = node->next;
+  /* Set this to NULL does matter */
+  node->next = node->prev = NULL;
+  list->elements--;
 
-      if(node->next)
-         node->next->prev = NULL;
-      else
-         list->tail = NULL;
-   }
-   else if(node == list->tail)
-   {
-      list->tail = list->tail->prev;
-      list->tail->next = NULL;
-   }
-   else
-   {
-      node->prev->next = node->next;
-      node->next->prev = node->prev;
-   }
-
-   list->elements--;
-   return node;
+  return node;
 }
 
-void libopm_list_free(OPM_LIST_T *list)
+void
+libopm_list_free(OPM_LIST_T *list)
 {
-   MyFree(list);
+  MyFree(list);
 }
 
-void libopm_node_free(OPM_NODE_T *node)
+void
+libopm_node_free(OPM_NODE_T *node)
 {
-   MyFree(node);
+  MyFree(node);
 }
