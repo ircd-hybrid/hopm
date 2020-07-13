6306beb580b8c1e27f913941c635039ec3a2b3ce
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2020-07-13 10:08:29 +0000

    - libopm: work towards ipv6 support; use sockaddr_storage; replace inet_pton() with getaddrinfo()
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/branches/1.1.x@9530 82007160-df01-0410-b94d-b575c5fd34c7

--- src/libopm/src/libopm.h
+++ src/libopm/src/libopm.h
@@ -29,7 +29,8 @@ typedef int OPM_PROXYREAD_T  (OPM_T *, OPM_SCAN_T *, OPM_CONNECTION_T *);
 
 struct _OPM_SCAN
 {
-  struct sockaddr_in   addr;           /* Address in byte order of remote client */
+  struct sockaddr_storage addr;        /* Address in byte order of remote client */
+  size_t addr_len;
   OPM_REMOTE_T        *remote;         /* Pointed to the OPM_REMOTE_T for this scan, passed by client */
   OPM_LIST_T           connections;    /* List of individual connections of this scan (1 for each protocol) */
 };
