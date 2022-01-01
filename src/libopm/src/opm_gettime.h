52fc426e8101c71b4a9d756554b4c9a1ef7f49e5
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2022-01-01 10:22:47 +0000

    - Bump copyright years
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/hopm/branches/1.1.x@10025 82007160-df01-0410-b94d-b575c5fd34c7

--- src/libopm/src/opm_gettime.h
+++ src/libopm/src/opm_gettime.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2014-2021 ircd-hybrid development team
+ *  Copyright (c) 2014-2022 ircd-hybrid development team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
